02e9df712a72a7453af5e69657f36678bf906c13
Ponja94 <henrique.danielb@gmail.com>
2022-04-18 19:09:43 -0300

    3rd commit

--- guessing_number.py
+++ guessing_number.py
@@ -1,6 +1,8 @@
 # Guessing the number
 
 import random
+import PySimpleGUI as sg
+
 
 class GuessNumber:
     def __init__(self):
@@ -10,26 +12,41 @@ class GuessNumber:
         self.try_again = True
 
     def Start(self):
+        # Layout
+        layout = [
+            [sg.Text('Your Guess', size=(39, 0))],
+            [sg.Input(size=(18, 0), key='GuessValuee')],
+            [sg.Button('Guess!')],
+            [sg.Output(size=(39, 10))]
+        ]
+        # Window
+        self.window = sg.Window('Guess a number!', layout=layout)
         self.RandomNumberGenerator()
-        self.AskRandomNumber()
-        while self.try_again is True:
-            if int(self.guess_value) > self.random_value:
-                print('lower')
-                self.AskRandomNumber()
-            elif int(self.guess_value) < self.random_value:
-                print('higher')
-                self.AskRandomNumber()
-            else:
-                print('CONGRATULATIONS!')
-                self.try_again = False
-
-    def AskRandomNumber(self):
-        self.guess_value = input('Guess a number: ')
+        try:
+            while True:
+                # Reciv values
+                self.event, self.value = self.window.Read()
+                # Do something with values
+                if self.event == 'Guess!':
+                    self.guess_value = self.value['GuessValuee']
+                    while self.try_again == True:
+                        if int(self.guess_value) > self.random_value:
+                            print('lower')
+                            break
+                        elif int(self.guess_value) < self.random_value:
+                            print('higher')
+                            break
+                        if int(self.guess_value) == self.random_value:
+                            print('CONGRATULATIONS!')
+                            self.try_again = False
+                            break
+        except:
+            print('Please enter only numbers')
+            self.Start()
 
     def RandomNumberGenerator(self):
         self.random_value = random.randint(self.min_value, self.max_value)
 
 
 guess = GuessNumber()
-
 guess.Start()
